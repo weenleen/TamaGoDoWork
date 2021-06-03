37e1676a4a965fc79a7df8ab1e2f11816a4b7cd7
soloplxya <69782911+soloplxya@users.noreply.github.com>
2021-06-03 23:00:13 +0800

    worked on layout of schedule fragment ( schedule_frag.xml )

--- app/src/main/java/com/example/tamagodowork/ScheduleFrag.java
+++ app/src/main/java/com/example/tamagodowork/ScheduleFrag.java
@@ -4,12 +4,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.api.Context;
+import com.google.api.Distribution;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 public class ScheduleFrag extends Fragment {
+
+    ImageButton NextButton, PreviousButton;
+    TextView CurrentDate;
+    GridView gridView;
+    private static final int MAX_CALENDAR_DAY = 42;
+    Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
+    Context context;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM yyyy", Locale.ENGLISH);
+    SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM", Locale.ENGLISH);
+    SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.ENGLISH);
+
+    List<Date> dates = new ArrayList<>();
+    List<Events> eventsList = new ArrayList<>();
 
     @Nullable
     @Override
