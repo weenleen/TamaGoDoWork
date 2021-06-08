e17269b3427632923e15680f2c4d912bcb559f66
soloplxya <69782911+soloplxya@users.noreply.github.com>
2021-06-08 21:34:23 +0800

    worked on scheduleFrag.java

--- app/src/main/java/com/example/tamagodowork/ScheduleFrag.java
+++ app/src/main/java/com/example/tamagodowork/ScheduleFrag.java
@@ -1,12 +1,18 @@
 package com.example.tamagodowork;
 
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,6 +27,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.TimeZone;
 
 public class ScheduleFrag extends Fragment {
 
@@ -35,6 +42,7 @@ public class ScheduleFrag extends Fragment {
     SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM", Locale.ENGLISH);
     SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.ENGLISH);
 
+    AlertDialog alertDialog;
     List<Date> dates = new ArrayList<>();
     List<Events> eventsList = new ArrayList<>();
 
@@ -51,7 +59,6 @@ public class ScheduleFrag extends Fragment {
         CurrentDate = view.findViewById(R.id.currentDate);
         gridView = view.findViewById(R.id.gridView);
 
-        // gridView.setAdapter();
 
         PreviousButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,6 +76,55 @@ public class ScheduleFrag extends Fragment {
             }
         });
 
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setCancelable(true);
+                View addView = LayoutInflater.from(parent.getContext()).inflate(R.layout.add_new_event, null);
+                EditText EventName = addView.findViewById(R.id.setEvent);
+                EditText EventTime = addView.findViewById(R.id.setTime);
+                Button AddEvent = addView.findViewById(R.id.add_new_evt_button);
+                EventTime.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Calendar calendar = Calendar.getInstance();
+                        int hours = calendar.get(Calendar.HOUR_OF_DAY);
+                        int minute = calendar.get(Calendar.MINUTE);
+                        TimePickerDialog timePickerDialog = new TimePickerDialog(addView.getContext(), R.style.Theme_AppCompat_Light_Dialog,
+                            new TimePickerDialog.OnTimeSetListener() {
+                                @Override
+                                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                        Calendar c = Calendar.getInstance();
+                                        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                                        c.set(Calendar.MINUTE, minute);
+                                        c.setTimeZone(TimeZone.getDefault());
+                                        SimpleDateFormat hourFormat = new SimpleDateFormat("K:mm a", Locale.ENGLISH);
+                                        String event_Time = hourFormat.format(c.getTime());
+                                        EventTime.setText(event_Time);
+
+
+
+                                }
+                            }, hours, minute, false);
+
+
+                    }
+                });
+
+                AddEvent.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+
+
+
+
+            }
+        });
+
         return view;
 
     }
@@ -76,5 +132,18 @@ public class ScheduleFrag extends Fragment {
     private void SetUpCalendar() {
         String currentDate = dateFormat.format(calendar.getTime());
         CurrentDate.setText(currentDate);
+        dates.clear();
+        Calendar monthCalendar = (Calendar) calendar.clone();
+        // sets the values for the calendar fields, YEAR, MONTH, DAY_OF_MONTH, HOUR_OF_DAY, MINUTE & SECOND
+        monthCalendar.set(Calendar.DAY_OF_MONTH,1);
+        int firstDayOfMonth = monthCalendar.get(Calendar.DAY_OF_MONTH) - 1;
+        // adds or subtracts the specified amount of time to the given calendar field, based on the calendar rules
+        // 2nd param : amount of time to be added to this field
+        monthCalendar.add(Calendar.DAY_OF_MONTH, -firstDayOfMonth);
+
+        while (dates.size() < MAX_CALENDAR_DAY) {
+            dates.add(monthCalendar.getTime());
+            monthCalendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
     }
 }
